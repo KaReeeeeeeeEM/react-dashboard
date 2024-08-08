8815d9291b3b909c7366f3669b5b7e9487a0aacf
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2024-08-08 11:21:09 +0300

    initialized a project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './output.css'
+
+function App() {
+
+  return (
+    <div className='w-screen h-screen bg-red-600'>
+        <h1 className='text-center text-white text-7xl'>Hello, Vite!</h1>
+    </div>
+  )
+}
+
+export default App
